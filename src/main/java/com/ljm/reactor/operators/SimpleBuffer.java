20a02bd8839d1057e11299c7148ec6dc6283a03a
lijiaming <lijiaming1984@gmail.com>
2018-04-19 23:34:57 +0800

    update buffer

--- src/main/java/com/ljm/reactor/operators/SimpleBuffer.java
+++ src/main/java/com/ljm/reactor/operators/SimpleBuffer.java
@@ -5,6 +5,7 @@ import reactor.test.StepVerifier;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 
 /**
  * buffer方法重载
@@ -18,9 +19,15 @@ public class SimpleBuffer {
     private static Flux<String> just = Flux.just("a", "b", "c", "d", "e");
 
     public static void main(String[] args) {
+        //使用Flux默认的ArrayList作为buffer
         buffer();
         bufferWithMax();
-        bufferWithMaxAndSkipAndMaxBiggerThanSkip();
+        bufferWithMaxBiggerThanSkip();
+        bufferWithSkipBiggerThanMax();
+        bufferWithMaxEqualsSkip();
+
+        //使用用户自定义的Collection作为buffer
+        bufferViaCollection();
         System.out.println("All test success.");
     }
 
@@ -41,14 +48,46 @@ public class SimpleBuffer {
                 .verifyComplete();
     }
 
-    private static void bufferWithMaxAndSkipAndMaxBiggerThanSkip() {
+    private static void bufferWithMaxBiggerThanSkip() {
         StepVerifier.create(
+                //buffer 为2，skip 为1，元素共5个，返回值下标为：
+                //（0,1) (1,2),(2,3),(3,4),(4)
                 just.buffer(2, 1)
         ).expectNext(new ArrayList<>(Arrays.asList("a", "b")))
                 .expectNext(new ArrayList<>(Arrays.asList("b", "c")))
                 .expectNext(new ArrayList<>(Arrays.asList("c", "d")))
                 .expectNext(new ArrayList<>(Arrays.asList("d", "e")))
                 .expectNext(new ArrayList<>(Arrays.asList("e")))
+                .verifyComplete();
+    }
+
+    private static void bufferWithSkipBiggerThanMax() {
+        StepVerifier.create(
+                //buffer 为2，skip 为3，元素共5个，返回值下标为：
+                //（0,1) (3,4)
+                just.buffer(2, 3)
+        ).expectNext(new ArrayList<>(Arrays.asList("a", "b")))
+                .expectNext(new ArrayList<>(Arrays.asList("d", "e")))
+                .verifyComplete();
+    }
+
+    private static void bufferWithMaxEqualsSkip() {
+        StepVerifier.create(
+                // buffer 为2，skip 为2，元素共5个，返回值下标为：
+                //（0,1) (2,3) (4)
+                // 即当maxSize等于skip时，等价于只有maxSize
+                just.buffer(2, 2)
+        ).expectNext(new ArrayList<>(Arrays.asList("a", "b")))
+                .expectNext(new ArrayList<>(Arrays.asList("c", "d")))
+                .expectNext(new ArrayList<>(Arrays.asList("e")))
+                .verifyComplete();
+    }
+
+    private static void bufferViaCollection() {
+        StepVerifier.create(
+                just.buffer(3, () -> new LinkedList<String>())
+        ).expectNext(new LinkedList<>(Arrays.asList("a", "b", "c")))
+                .expectNext(new LinkedList<>(Arrays.asList("d", "e")))
                 .verifyComplete();
     }
 }
