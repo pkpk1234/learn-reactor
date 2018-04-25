1ca1da081cc14d1b08ca73d642391623b7ed7b52
lijiaming <lijiaming1984@gmail.com>
2018-04-25 22:13:12 +0800

    update

---
+++ src/main/java/com/ljm/reactor/scheduler/FluxPublishOnThreadSwitch.java
@@ -0,0 +1,35 @@
+package com.ljm.reactor.scheduler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @author 李佳明 https://github.com/pkpk1234
+ * @date 2018-04-25
+ */
+public class FluxPublishOnThreadSwitch {
+
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        Flux.range(1, 20)
+                .map(i -> {
+                    System.out.println("map in Thread " + Thread.currentThread().getName() +" value is " + i);
+                    return ++i;
+                })
+                //使用Schedulers.parallel()线程池执行之后的操作
+                .publishOn(Schedulers.parallel())
+                .doOnComplete(() -> countDownLatch.countDown())
+                .subscribe(i -> {
+                    System.out.println("Current Thread is "
+                            + Thread.currentThread().getName() + ", value " + i);
+                });
+        //如果使用了Scheduler，则subscribe是异步的，主线程必须阻塞才行
+        System.out.println(Thread.currentThread().getName() + "-Main thread blocking");
+        countDownLatch.await();
+        System.out.println(Thread.currentThread().getName() + "-Flow complete,Main thread run and finished");
+    }
+}
