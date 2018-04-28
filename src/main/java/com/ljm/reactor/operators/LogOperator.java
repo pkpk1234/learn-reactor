3f866c7fb0b3ad3d3e3f4eca1b506bd81d95418c
lijiaming <lijiaming1984@gmail.com>
2018-04-29 00:00:53 +0800

    update log

--- src/main/java/com/ljm/reactor/operators/LogOperator.java
+++ src/main/java/com/ljm/reactor/operators/LogOperator.java
@@ -1,6 +1,9 @@
 package com.ljm.reactor.operators;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.SignalType;
+
+import java.util.logging.Level;
 
 /**
  * @author 李佳明 https://github.com/pkpk1234
@@ -11,6 +14,16 @@ public class LogOperator {
         Flux.just(1, 2, 3, 4, 5)
                 //日志记录详细的执行步骤
                 .log()
+                .subscribe();
+
+        Flux.just(1, 2, 3, 4, 5)
+                //使用自定义日志配置
+                .log("myCategory")
+                .subscribe();
+
+        Flux.just(1, 2, 3, 4, 5)
+                //使用自定义日志配置，仅仅关注onComplete信号
+                .log("myCategory", Level.WARNING, SignalType.ON_COMPLETE)
                 .subscribe();
     }
 }
