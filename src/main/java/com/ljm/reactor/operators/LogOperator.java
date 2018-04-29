c01f9c30202daa17193124f0b0481a9bbae2f0b5
lijiaming <lijiaming1984@gmail.com>
2018-04-29 22:02:55 +0800

    update timeOp

--- src/main/java/com/ljm/reactor/operators/LogOperator.java
+++ src/main/java/com/ljm/reactor/operators/LogOperator.java
@@ -23,6 +23,7 @@ public class LogOperator {
 
         Flux.just(1, 2, 3, 4, 5)
                 //使用自定义日志配置，仅仅关注onComplete信号
+                //注意Level类型是java.util.logging.Level
                 .log("myCategory", Level.WARNING, SignalType.ON_COMPLETE)
                 .subscribe();
     }
--- src/main/java/com/ljm/reactor/operators/TimestampOperator.java
+++ src/main/java/com/ljm/reactor/operators/TimestampOperator.java
@@ -1,14 +1,16 @@
 package com.ljm.reactor.operators;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Flux;
-import reactor.core.scheduler.Schedulers;
 import reactor.util.function.Tuple2;
 
 /**
  * @author 李佳明 https://github.com/pkpk1234
  * @date 2018-04-29
  */
-public class ElapsedOperator {
+public class TimestampOperator {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TimestampOperator.class);
 
     public static void main(String[] args) {
         Flux<Integer> sourceFlux = Flux.range(0, 5)
@@ -21,13 +23,14 @@ public class ElapsedOperator {
                     }
                     return integer;
                 });
+
         /**
          * elapsed之后返回Flux<Tuple2<Long, Integer>>，Tuple2.getT1()返回
          * 耗时，Tuple2.getT2()返回数据值
-         * 如果使用log，则会打印出信号、耗时和数据值
+         * 如果使用log，则会打印出信号、当前时钟时间和数据值
          */
-
-        Flux<Tuple2<Long, Integer>> timedFlux = sourceFlux.elapsed();
+        Flux<Tuple2<Long, Integer>> timedFlux = sourceFlux.timestamp();
+        LOGGER.info("current clock time is {} ", System.currentTimeMillis());
         timedFlux.log().subscribe();
     }
 }
