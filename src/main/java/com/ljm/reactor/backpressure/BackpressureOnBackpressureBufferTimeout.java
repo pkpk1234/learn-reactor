572c5ff60ff2256ef00c19d769447821f9b169d4
lijiaming <lijiaming1984@gmail.com>
2018-05-20 17:58:25 +0800

    update

---
+++ src/main/java/com/ljm/reactor/backpressure/BackpressureOnBackpressureBufferTimeout.java
@@ -0,0 +1,53 @@
+package com.ljm.reactor.backpressure;
+
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.UnicastProcessor;
+
+import java.time.Duration;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.IntStream;
+
+/**
+ * @author 李佳明 https://github.com/pkpk1234
+ * @date 2018-05-20
+ */
+public class BackpressureOnBackpressureBufferTimeout {
+    public static void main(String[] args) throws InterruptedException {
+
+        UnicastProcessor<String> hotSource = UnicastProcessor.create();
+        Flux<String> hotFlux = hotSource
+                .publish()
+                .autoConnect()
+                .onBackpressureBuffer(Duration.ofMillis(500), 5, s -> {
+                });
+
+        CompletableFuture future = CompletableFuture.runAsync(() -> {
+            IntStream.range(0, 50).forEach(
+                    value -> {
+                        hotSource.onNext("value is " + value);
+                    }
+            );
+        });
+
+        BaseSubscriber<String> subscriber = new BaseSubscriber<String>() {
+            @Override
+            protected void hookOnSubscribe(Subscription subscription) {
+                request(20);
+            }
+
+            @Override
+            protected void hookOnNext(String value) {
+                System.out.println("get value " + value);
+
+            }
+        };
+        hotFlux.subscribe(subscriber);
+        future.join();
+        Thread.sleep(1000);
+        System.out.println("get rest elements from buffer");
+        //再次获取10个元素，根据策略应返还最后的10个元素
+        subscriber.request(10);
+    }
+}
