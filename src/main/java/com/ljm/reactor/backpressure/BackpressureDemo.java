c1d3c6c8b9cb91f52daa45da891124c1732991fc
lijiaming <lijiaming1984@gmail.com>
2018-05-20 09:01:35 +0800

    update demo

--- src/main/java/com/ljm/reactor/backpressure/BackpressureDemo.java
+++ src/main/java/com/ljm/reactor/backpressure/BackpressureDemo.java
@@ -2,6 +2,7 @@ package com.ljm.reactor.backpressure;
 
 import reactor.core.publisher.Flux;
 
+import java.time.Duration;
 import java.util.concurrent.CountDownLatch;
 
 /**
@@ -12,13 +13,16 @@ public class BackpressureDemo {
     public static void main(String[] args) throws InterruptedException {
         CountDownLatch countDownLatch = new CountDownLatch(1);
         //可以观察到明显的限流
-        Flux<Integer> flux = Flux.range(0, 50).doOnComplete(() -> countDownLatch.countDown());
+        Flux<Long> flux = Flux.interval(Duration.ofMillis(50))
+                .take(50)
+                .doOnComplete(() -> countDownLatch.countDown());
         flux.subscribe(new MyLimitedSubscriber(5));
         countDownLatch.await();
 
         //使用比count还大的limiter，相当于不限流
         System.out.println("use big limiter");
-        Flux.range(0, 50)
+        Flux.interval(Duration.ofMillis(50))
+                .take(50)
                 .subscribe(new MyLimitedSubscriber(100));
     }
 }
